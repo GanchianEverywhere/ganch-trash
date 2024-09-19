c1a6a83d9a5ddc196f4cdb583e11b439319894eb
Ganchian <54121190+GanchianEverywhere@users.noreply.github.com>
2024-09-19 18:16:29 -0500

    main

---
+++ config.lua
@@ -0,0 +1,5 @@
+Config = {}
+
+Config.TrashModels = {
+    'prop_cleaning_trolly', 'prop_dumpster_4a', 'prop_dumpster_01a'
+}
--- ganch-trash/config.lua
+++
@@ -1,5 +0,0 @@
-Config = {}
-
-Config.TrashModels = {
-    'prop_cleaning_trolly', 'prop_dumpster_4a', 'prop_dumpster_01a'
-}
